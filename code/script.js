a9976fb5b7cf28cd6e097e5033ab2ffb27933f98
melih <9021785@student.zadkine.nl>
2025-05-26 12:45:31 +0200

    Add files via upload

---
+++ code/script.js
@@ -0,0 +1,11 @@
+function startGame() {
+  alert("Je avontuur begint! (Klik op een testknop om te winnen of verliezen)");
+}
+
+function goToWin() {
+  window.location.href = "win.html";
+}
+
+function goToLose() {
+  window.location.href = "lose.html";
+}
